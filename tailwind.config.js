f9640f3e680cb9f0b290e69ef35e1467902c1082
kaduhiro <kaduhiro.com@gmail.com>
2022-06-23 11:11:14 +0000

    feat: フェードインフェードアウトのアニメーションを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,69 @@ module.exports = {
       zIndex: {
         '-1': '-1',
       },
+      keyframes: {
+        'fade-out': {
+          '0%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+          '99%': {
+            opacity: '0',
+            transform: 'translateY(0)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateY(100%)',
+          },
+        },
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-down': {
+          from: {
+            opacity: '1',
+            transform: 'translateY(0px)',
+          },
+          to: {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+        },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'fade-out-up': {
+          from: {
+            opacity: '1',
+            transform: 'translateY(0px)',
+          },
+          to: {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+        },
+      },
+      animation: {
+        'fade-out': 'fade-out 0.5s ease-in-out forwards',
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-out-down': 'fade-out-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
+        'fade-out-up': 'fade-out-up 0.5s ease-out',
+      },
     },
   },
   variants: {},
