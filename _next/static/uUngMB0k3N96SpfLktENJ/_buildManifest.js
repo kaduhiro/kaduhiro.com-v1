80403aff354c14237baad3d48249335d102d9108
kaduhiro <kaduhiro@users.noreply.github.com>
2022-10-22 00:27:13 +0000

    deploy: c4a069a216e45c2314d9a17bfd1a4b14fb52ee06

---
+++ _next/static/uUngMB0k3N96SpfLktENJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/579-6c991714a5a2026a.js","static/chunks/pages/index-f20a49f74f60be56.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
